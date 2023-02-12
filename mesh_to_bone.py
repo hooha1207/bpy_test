388447e45cdbdfe6e47818048b8d801587950cbd
hooha1207 <68090101+hooha1207@users.noreply.github.com>
2023-02-12 16:50:18 +0900

    Update mesh_to_bone.py

--- mesh_to_bone.py
+++ mesh_to_bone.py
@@ -1,20 +1,76 @@
 import bpy
 import bmesh
+import math
 
 
-index = 5 # here the index you want select please change 
+#bpy.ops.object.armature_add(enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+
+actOb = bpy.context.object
+org_co = bpy.context.object.matrix_world.to_translation()
+actOb_sc = bpy.context.object.matrix_world.to_scale()
+actOb_rt = bpy.context.object.matrix_world.to_euler()
+
+vertList = [((actOb_sc*vertex.co) + org_co) for vertex in actOb.data.vertices if vertex.select]
+
+#oa = bpy.context.active_object
+
+
+
 
 obj = bpy.context.object
 me = obj.data
+bpy.ops.object.mode_set(mode='EDIT')
 bm = bmesh.from_edit_mesh(me)
-
 vertices= [e for e in bm.verts]
-oa = bpy.context.active_object
 
-for vert in vertices:
-    if vert.index == index:
-        vert.select = True
-    else:
-        vert.select = False
+vertices_idx= [e.index for e in bm.verts]
+vertices_co= [((actOb_sc * e.co) + org_co) for e in bm.verts]
+
+
+#index = 5 # here the index you want select please change 
+#for vert in vertices:
+#    if vert.index == index:
+#        vert.select = True
+#    else:
+#        vert.select = False
 
 bmesh.update_edit_mesh(me)
+bpy.ops.object.mode_set(mode='OBJECT')
+
+
+
+
+
+add_amarture_name = "add_amarture"
+
+bpy.ops.object.armature_add(enter_editmode=False, align='WORLD', location=(0,0,0), scale=(1, 1, 1))
+bpy.context.object.name = add_amarture_name
+
+
+bpy.ops.object.mode_set(mode='EDIT')
+bpy.ops.armature.select_all(action='SELECT')
+bpy.ops.armature.delete()
+
+
+for idx, i in enumerate(vertices_co):
+    add_bone_name = f"shape_to_bone{idx}"
+    bpy.ops.armature.bone_primitive_add()
+    bpy.ops.armature.select_linked()
+    bpy.ops.transform.translate(value=i) #world_space_coordinate
+
+    bpy.ops.object.mode_set(mode='OBJECT')
+    bpy.context.view_layer.objects.active = actOb
+    
+    vertex_idx = [vertices_idx[idx]]
+    v_group = bpy.context.object.vertex_groups.new(name=add_bone_name) #be wary of vertex group has no duplicate
+    v_group.add(vertex_idx, 1.0, "REPLACE")
+    
+    bpy.context.view_layer.objects.active = bpy.context.scene.objects.get(add_amarture_name)
+    bpy.ops.object.mode_set(mode='EDIT')
+    
+
+bpy.ops.object.mode_set(mode='OBJECT')
+bpy.context.view_layer.objects.active = actOb
+bpy.ops.object.modifier_add(type='ARMATURE')
+bpy.context.object.modifiers["Armature"].object = bpy.data.objects[add_amarture_name]
+bpy.context.view_layer.objects.active = bpy.context.scene.objects.get(add_amarture_name)
