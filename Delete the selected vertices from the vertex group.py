edbabb3dbf2a023a75da6ddfb58168b8967bc8b1
hooha1207 <68090101+hooha1207@users.noreply.github.com>
2023-04-06 16:59:02 +0900

    Update Delete the selected vertices from the vertex group.py

--- Delete the selected vertices from the vertex group.py
+++ Delete the selected vertices from the vertex group.py
@@ -16,3 +16,7 @@ for ob in selOb:
 # how to use
 # 오브젝트의 edit mode로 진입한 뒤 모든 vertex group에서 weight를 삭제하고자 하는 vertex 선택
 # 해당 코드 실행
+
+# 혹은 edit mode에서 모든 vertex group에서 값을 삭제할 vertex를 선택한 뒤 
+# F3을 눌러 remove from all 을 검색해서 실행하기
+# 이럼 위 코드를 실행한 것과 같은 결과를 얻음
