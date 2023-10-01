3892407a50ba7eed3cc3c3fa6f6a3f2b647c27f7
hooha1207 <hooha1207@gmail.com>
2023-10-02 00:30:51 +0900

    Update bpy_bmesh_technote.py

--- bpy_bmesh_technote.py
+++ bpy_bmesh_technote.py
@@ -295,7 +295,9 @@ import subprocess
 import sys
 import os
 python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
+# C:\Program Files\blender3-0\3.0\python\bin\python.exe
 target = os.path.join(sys.prefix, 'lib', 'site-packages')
+# C:\Program Files\blender3-0\3.0\python\lib\site-packages
 subprocess.call([python_exe, '-m', 'ensurepip'])
 subprocess.call([python_exe, '-m', 'pip', 'install', '--upgrade', 'pip'])
 subprocess.call([python_exe, '-m', 'pip', 'install', '--upgrade', 'scipy', '-t', target])
