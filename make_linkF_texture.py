a1abf1761a5b48ad0a87ca9b4a6eddf33bb7a44a
hooha1207 <68090101+hooha1207@users.noreply.github.com>
2024-08-28 20:18:59 +0900

    Create make_linkF_texture.py

---
+++ make_linkF_texture.py
@@ -0,0 +1,63 @@
+import bpy
+import bmesh
+import numpy as np
+
+
+
+tmp_vvs_l = 99
+l_attr_n = 'linkF_v.'
+link_level = 2
+
+
+
+
+def get_linkF_v(vs, rvs, obm):
+    for v in vs:
+        v = obm.verts[v]
+        for f in v.link_faces:
+            for vv in f.verts:
+                if not vv.index in rvs:
+                    rvs.append(vv.index)
+    
+
+
+for ob in bpy.context.selected_objects:
+    obm = bmesh.new()
+    obm.from_mesh(ob.data)
+    obm.verts.ensure_lookup_table()
+    obm.faces.ensure_lookup_table()
+    obm.edges.ensure_lookup_table()
+    
+    ob_vvs = []
+    vvs_l = 0
+    for v in obm.verts:
+        vvs = []
+        for vlf in v.link_faces:
+            for vv in vlf.verts:
+                if not vv.index in vvs:
+                    vvs.append(vv.index)
+        
+        if link_level>0:
+            for _ in range(link_level):
+                get_linkF_v(vvs.copy(), vvs, obm)
+        
+        vvs.remove(v.index)
+        
+        vvs_arr = np.zeros(tmp_vvs_l, dtype=np.int32)
+        ob_vvs.append(vvs_arr)
+        
+        
+        tmp = 0
+        for vid in vvs:
+            vvs_arr[tmp] = vid
+            tmp+=1
+        if len(vvs) > vvs_l:
+            vvs_l = len(vvs)
+    
+    ob_vvs_arr = np.array(ob_vvs)
+    ob_vvs_arr = ob_vvs_arr[:,:vvs_l]
+    
+    for i in range(vvs_l):
+        attr = ob.data.attributes.new(f'{l_attr_n}{i}', 'INT', 'POINT')
+        for idx,v in enumerate(attr.data):
+            v.value = ob_vvs_arr[idx][i]
